099e05c164aae6b7b4cb82a264ccdd215d93e0a5
gimjinnyeong <wlssud0701@naver.com>
2023-04-18 20:31:42 -0700

    Initial Commit

---
+++ unieat_ios/unieat_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  unieat_iosApp.swift
+//  unieat_ios
+//
+//  Created by 김진녕 on 2023/04/18.
+//
+
+import SwiftUI
+
+@main
+struct unieat_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
